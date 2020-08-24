f2e7158f08a6034041b4b7757c701a68e9226562
Elton Fonseca <elton869@gmail.com>
2020-08-24 13:44:23 -0300

    11 - Criando tabela, model e controller do cadastro de empresas

---
+++ projeto/database/migrations/2020_08_24_133428_create_empresas_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->id();
+
+            $table->char('tipo', 10);
+
+            $table->string('nome');
+            $table->string('razao_social')->nullable();
+            $table->char('documento', 14);
+            $table->char('ie_rg', 25)->nullable();
+
+            $table->string('nome_contato', 100);
+            $table->char('celular', 9);
+            $table->string('email', 100);
+            $table->char('telefone', 9)->nullable();
+
+            $table->char('cep', 8);
+            $table->string('logradouro', 100);
+            $table->string('cidade', 50);
+            $table->char('estado', 2);
+
+            $table->text('observacao')->nullable();            
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+}
