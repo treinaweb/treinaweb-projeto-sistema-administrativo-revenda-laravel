300230d693eec1862afdfa8b23a3c0fc2c71b747
Elton Fonseca <elton869@gmail.com>
2020-08-24 14:53:00 -0300

    13 - Listando registros na view

--- projeto/app/Http/Controllers/EmpresaController.php
+++ projeto/app/Http/Controllers/EmpresaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Empresa;
 use Illuminate\Http\Request;
 
 class EmpresaController extends Controller
@@ -13,7 +14,9 @@ class EmpresaController extends Controller
      */
     public function index()
     {
-        return view('empresa.index');
+        $empresas = Empresa::paginate(1);
+
+        return view('empresa.index', \compact('empresas'));
     }
 
     /**
--- projeto/resources/views/empresa/index.blade.php
+++ projeto/resources/views/empresa/index.blade.php
@@ -26,65 +26,30 @@
                     <table class="table">
                         <thead>
                             <tr>
-                            <th style="width: 10px">#</th>
-                            <th>Task</th>
-                            <th>Progress</th>
-                            <th style="width: 40px">Label</th>
+                                <th style="width: 10px">#</th>
+                                <th>Nome Empresa</th>
+                                <th>Nome Contato</th>
+                                <th>Celular</th>
+                                <th>Ações</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                            <td>1.</td>
-                            <td>Update software</td>
-                            <td>
-                                <div class="progress progress-xs">
-                                <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                                </div>
-                            </td>
-                            <td><span class="badge bg-danger">55%</span></td>
-                            </tr>
-                            <tr>
-                            <td>2.</td>
-                            <td>Clean database</td>
-                            <td>
-                                <div class="progress progress-xs">
-                                <div class="progress-bar bg-warning" style="width: 70%"></div>
-                                </div>
-                            </td>
-                            <td><span class="badge bg-warning">70%</span></td>
-                            </tr>
-                            <tr>
-                            <td>3.</td>
-                            <td>Cron job running</td>
-                            <td>
-                                <div class="progress progress-xs progress-striped active">
-                                <div class="progress-bar bg-primary" style="width: 30%"></div>
-                                </div>
-                            </td>
-                            <td><span class="badge bg-primary">30%</span></td>
-                            </tr>
-                            <tr>
-                            <td>4.</td>
-                            <td>Fix and squish bugs</td>
-                            <td>
-                                <div class="progress progress-xs progress-striped active">
-                                <div class="progress-bar bg-success" style="width: 90%"></div>
-                                </div>
-                            </td>
-                            <td><span class="badge bg-success">90%</span></td>
-                            </tr>
+                            @foreach($empresas as $empresa)
+                                <tr>
+                                    <td>{{ $empresa->id }}</td>
+                                    <td>{{ $empresa->nome }}</td>
+                                    <td>{{ $empresa->nome_contato }}</td>
+                                    <td>{{ $empresa->celular }}</td>
+                                    <td><a href="" class="btn btn-primary">Detalhes</a></td>
+                                    
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
 
                 <div class="card-footer clearfix">
-                    <ul class="pagination pagination-md m-0 float-right">
-                        <li class="page-item"><a class="page-link" href="#">«</a></li>
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item"><a class="page-link" href="#">»</a></li>
-                    </ul>
+                    {{ $empresas->links() }}
               </div>
             </div>
         </div>
