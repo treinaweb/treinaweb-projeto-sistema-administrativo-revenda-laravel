120a535e7d5ef2cf46824876b54555a30b378802
Elton Fonseca <elton869@gmail.com>
2020-08-28 09:48:41 -0300

    33 - Protegendo as rotas com autenticação

--- projeto/routes/web.php
+++ projeto/routes/web.php
@@ -15,11 +15,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'Auth\LoginController@showLoginForm');
 
-Auth::routes();
+Auth::routes([
+    'register' => false
+]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')->group(function() {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('empresas', 'EmpresaController');
+    Route::resource('produtos', 'ProdutosController');
+    Route::resource('users', 'UsersController');
+});
 
-Route::resource('empresas', 'EmpresaController');
-
-Route::resource('produtos', 'ProdutosController');
-Route::resource('users', 'UsersController');
