5d93626a05d2838fe05cb5af00cebd7fd9088593
Elton Fonseca <elton869@gmail.com>
2020-08-28 09:58:39 -0300

    34 - Criando usuário via seeder

---
+++ projeto/database/seeds/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('123123123'),
+        ]);
+    }
+}
